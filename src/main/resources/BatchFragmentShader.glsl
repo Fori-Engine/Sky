349098c639e292286aa2af0afc543d0c2d7571cd
skyloft7 <skyloftmb@gmail.com>
2024-03-16 12:28:47 -0400

    modified vertex/fragment bloom shaders

--- src/main/resources/BatchFragmentShader.glsl
+++ src/main/resources/BatchFragmentShader.glsl
@@ -7,6 +7,8 @@ uniform sampler2D u_textures[32];
 in float f_texindex;
 in vec4 f_color;
 in float f_thickness;
+in float f_bloom;
+
 //gl_FragCoord goes y-axis up, but the engine uses y-axis down...
 layout(origin_upper_left) in vec4 gl_FragCoord;
 
@@ -32,38 +34,46 @@ void main()
 
     }
     else {
-        if(index == 0) FragColor = texture(u_textures[0], f_uv) * f_color;
-        if(index == 1) FragColor = texture(u_textures[1], f_uv) * f_color;
-        if(index == 2) FragColor = texture(u_textures[2], f_uv) * f_color;
-        if(index == 3) FragColor = texture(u_textures[3], f_uv) * f_color;
-        if(index == 4) FragColor = texture(u_textures[4], f_uv) * f_color;
-        if(index == 5) FragColor = texture(u_textures[5], f_uv) * f_color;
-        if(index == 6) FragColor = texture(u_textures[6], f_uv) * f_color;
-        if(index == 7) FragColor = texture(u_textures[7], f_uv) * f_color;
-        if(index == 8) FragColor = texture(u_textures[8], f_uv) * f_color;
-        if(index == 9) FragColor = texture(u_textures[9], f_uv) * f_color;
-        if(index == 10) FragColor = texture(u_textures[10], f_uv) * f_color;
-        if(index == 11) FragColor = texture(u_textures[11], f_uv) * f_color;
-        if(index == 12) FragColor = texture(u_textures[12], f_uv) * f_color;
-        if(index == 13) FragColor = texture(u_textures[13], f_uv) * f_color;
-        if(index == 14) FragColor = texture(u_textures[14], f_uv) * f_color;
-        if(index == 15) FragColor = texture(u_textures[15], f_uv) * f_color;
-        if(index == 16) FragColor = texture(u_textures[16], f_uv) * f_color;
-        if(index == 17) FragColor = texture(u_textures[17], f_uv) * f_color;
-        if(index == 18) FragColor = texture(u_textures[18], f_uv) * f_color;
-        if(index == 19) FragColor = texture(u_textures[19], f_uv) * f_color;
-        if(index == 20) FragColor = texture(u_textures[20], f_uv) * f_color;
-        if(index == 21) FragColor = texture(u_textures[21], f_uv) * f_color;
-        if(index == 22) FragColor = texture(u_textures[22], f_uv) * f_color;
-        if(index == 23) FragColor = texture(u_textures[23], f_uv) * f_color;
-        if(index == 24) FragColor = texture(u_textures[24], f_uv) * f_color;
-        if(index == 25) FragColor = texture(u_textures[25], f_uv) * f_color;
-        if(index == 26) FragColor = texture(u_textures[26], f_uv) * f_color;
-        if(index == 27) FragColor = texture(u_textures[27], f_uv) * f_color;
-        if(index == 28) FragColor = texture(u_textures[28], f_uv) * f_color;
-        if(index == 29) FragColor = texture(u_textures[29], f_uv) * f_color;
-        if(index == 30) FragColor = texture(u_textures[30], f_uv) * f_color;
-        if(index == 31) FragColor = texture(u_textures[31], f_uv) * f_color;
+
+        vec4 color;
+
+
+        if(index == 0) color = texture(u_textures[0], f_uv) * f_color;
+        if(index == 1) color = texture(u_textures[1], f_uv) * f_color;
+        if(index == 2) color = texture(u_textures[2], f_uv) * f_color;
+        if(index == 3) color = texture(u_textures[3], f_uv) * f_color;
+        if(index == 4) color = texture(u_textures[4], f_uv) * f_color;
+        if(index == 5) color = texture(u_textures[5], f_uv) * f_color;
+        if(index == 6) color = texture(u_textures[6], f_uv) * f_color;
+        if(index == 7) color = texture(u_textures[7], f_uv) * f_color;
+        if(index == 8) color = texture(u_textures[8], f_uv) * f_color;
+        if(index == 9) color = texture(u_textures[9], f_uv) * f_color;
+        if(index == 10) color = texture(u_textures[10], f_uv) * f_color;
+        if(index == 11) color = texture(u_textures[11], f_uv) * f_color;
+        if(index == 12) color = texture(u_textures[12], f_uv) * f_color;
+        if(index == 13) color = texture(u_textures[13], f_uv) * f_color;
+        if(index == 14) color = texture(u_textures[14], f_uv) * f_color;
+        if(index == 15) color = texture(u_textures[15], f_uv) * f_color;
+        if(index == 16) color = texture(u_textures[16], f_uv) * f_color;
+        if(index == 17) color = texture(u_textures[17], f_uv) * f_color;
+        if(index == 18) color = texture(u_textures[18], f_uv) * f_color;
+        if(index == 19) color = texture(u_textures[19], f_uv) * f_color;
+        if(index == 20) color = texture(u_textures[20], f_uv) * f_color;
+        if(index == 21) color = texture(u_textures[21], f_uv) * f_color;
+        if(index == 22) color = texture(u_textures[22], f_uv) * f_color;
+        if(index == 23) color = texture(u_textures[23], f_uv) * f_color;
+        if(index == 24) color = texture(u_textures[24], f_uv) * f_color;
+        if(index == 25) color = texture(u_textures[25], f_uv) * f_color;
+        if(index == 26) color = texture(u_textures[26], f_uv) * f_color;
+        if(index == 27) color = texture(u_textures[27], f_uv) * f_color;
+        if(index == 28) color = texture(u_textures[28], f_uv) * f_color;
+        if(index == 29) color = texture(u_textures[29], f_uv) * f_color;
+        if(index == 30) color = texture(u_textures[30], f_uv) * f_color;
+        if(index == 31) color = texture(u_textures[31], f_uv) * f_color;
+
+
+
+        FragColor = color;
     }
 
 }
--- src/main/resources/BatchVertexShader.glsl
+++ src/main/resources/BatchVertexShader.glsl
@@ -4,6 +4,8 @@ layout (location = 1) in vec2 v_uv;
 layout (location = 2) in float v_texindex;
 layout (location = 3) in vec4 v_color;
 layout (location = 4) in float v_thickness;
+layout (location = 5) in float v_bloom;
+
 
 
 
@@ -11,9 +13,8 @@ layout (location = 4) in float v_thickness;
 out vec2 f_uv;
 out float f_texindex;
 out vec4 f_color;
-out vec2 f_origin;
-out vec2 f_size;
 out float f_thickness;
+out float f_bloom;
 
 uniform mat4 v_model;
 uniform mat4 v_view;
@@ -30,6 +31,8 @@ void main() {
     f_uv = v_uv;
     f_color = v_color;
     f_thickness = v_thickness;
+    f_bloom = v_bloom;
+
 
 
     gl_Position = v_projection * v_view * v_model * vec4(v_pos, 0.0, 1.0);
