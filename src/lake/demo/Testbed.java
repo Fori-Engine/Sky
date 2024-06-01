e18ca6a6a0de42ed339e5e24cc2a5d6ceea06067
skyloft7 <skyloftmb@gmail.com>
2024-05-31 22:03:11 -0400

    Cleaned up test bed

--- src/lake/demo/Testbed.java
+++ src/lake/demo/Testbed.java
@@ -1,42 +1,26 @@
 package lake.demo;
 
-import lake.FileReader;
 import lake.FlightRecorder;
-import lake.Time;
 import lake.asset.AssetPack;
 import lake.asset.AssetPacks;
-import lake.asset.TextureData;
 import lake.graphics.*;
-import lake.graphics.vulkan.VulkanShaderProgram;
-import lake.graphics.vulkan.VulkanTexture2D;
-import lake.physics.CircleBody2D;
-import lake.physics.RectBody2D;
-import lake.physics.RigidBody2D;
-import lake.physics.World;
-import org.joml.Matrix3x2f;
 import org.joml.Matrix4f;
-import org.joml.Vector2f;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Random;
 
 import static lake.graphics.vulkan.VulkanRenderer2D.TOTAL_SIZE_BYTES;
 
-public class PlatformerDemo {
+public class Testbed {
 
-
-    static float t = 0;
-    static float elapsedTime = 0;
-    static int currentIndex = 0;
     public static void main(String[] args) throws InterruptedException {
         AssetPacks.open("core", AssetPack.openLocal(new File("assets")));
 
         FlightRecorder.setEnabled(true);
-        FlightRecorder.meltdown(PlatformerDemo.class, "This is very very bad");
-        FlightRecorder.error(PlatformerDemo.class, "This is very bad");
-        FlightRecorder.info(PlatformerDemo.class, "This isn't bad");
-        FlightRecorder.todo(PlatformerDemo.class, "I'm just being lazy");
+        FlightRecorder.meltdown(Testbed.class, "This is very very bad");
+        FlightRecorder.error(Testbed.class, "This is very bad");
+        FlightRecorder.info(Testbed.class, "This isn't bad");
+        FlightRecorder.todo(Testbed.class, "I'm just being lazy");
 
 
 
@@ -135,29 +119,24 @@ public class PlatformerDemo {
             }
         }
 
+        ArrayList<Boolean> circles = new ArrayList<>();
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                circles.add(Math.random() >= 0.5f);
+            }
+        }
 
 
 
-        Random random = new Random();
-        Matrix3x2f shear = new Matrix3x2f();
-        shear.m10 = 1;
 
 
 
-        float epsilon = 0.01f;
-
-        //Transforms, Textures, lots to think about
         while(!window.shouldClose()){
 
 
             renderer2D.clear(Color.WHITE);
 
-            elapsedTime += Time.deltaTime();
-            t = (float) ((float) (Math.toRadians(45) * Math.sin(((2 * Math.PI) / 3) * elapsedTime)) + Math.toRadians(45));
 
-            if(t >= Math.toRadians(90 - epsilon)){
-                currentIndex = random.nextInt(5 * 5);
-            }
 
 
             renderer2D.startBatch(shaderProgram1);
@@ -166,21 +145,7 @@ public class PlatformerDemo {
 
                 for (int y = 0; y < 5; y++) {
                     for (int x = 0; x < 5; x++) {
-
-                        int myIndex = (y * 5) + x;
-
-                        float thickness = 0.5f;
-
-                        if (myIndex == currentIndex) {
-
-                            renderer2D.setOrigin((x * width) + width / 2f, (y * height) + height / 2f);
-                            thickness = (float) ((1 / Math.toRadians(180f)) * (t + Math.toRadians(90)));
-
-                        }
-
-                        renderer2D.drawEllipse(x * width, y * height, width, height, colors.get((y * 5) + x), thickness);
-                        renderer2D.resetTransform();
-                        renderer2D.setOrigin(0, 0);
+                        renderer2D.drawFilledRect(x * width, y * height, width, height, colors.get((y * 5) + x));
                     }
 
                 }
@@ -190,17 +155,23 @@ public class PlatformerDemo {
             renderer2D.startBatch(shaderProgram2);
             {
 
-                renderer2D.setTransform(new Matrix4f().identity().mul(shear));
+
 
                 int w = 60, h = 60;
 
                 for (int y = 0; y < 10; y++) {
                     for (int x = 0; x < 10; x++) {
-                        renderer2D.drawFilledRect(((x * w) + 1 + 300), ((y * h) + 1 + 150), w - 2, h - 2, Color.WHITE);
+
+                        Boolean b = circles.get((y * 5) + x);
+
+                        if(b)
+                            renderer2D.drawEllipse(((x * w) + 1 + 300), ((y * h) + 1 + 150), w - 2, h - 2, Color.WHITE, 0.5f);
+                        else
+                            renderer2D.drawRect(((x * w) + 1 + 300), ((y * h) + 1 + 150), w - 2, h - 2, Color.WHITE, 15);
                     }
                 }
 
-                renderer2D.resetTransform();
+
             }
             renderer2D.endBatch();
 
